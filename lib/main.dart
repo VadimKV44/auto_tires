e9f8cdcb53dad60964bc70073ac4555b8b25ea0a
ZikFreeDKarteR44 <john.smithkarter2016@mail.ru>
2023-10-06 15:23:12 +0300

    Refinement of the splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:auto_tires/View/screen/splash_screen.dart';
+import 'package:auto_tires/View/utils/my_custom_scroll_behavior.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,8 +11,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: SplashScreen(),
+    return MaterialApp(
+      scrollBehavior: MyCustomScrollBehavior(),
+      home: const SplashScreen(),
     );
   }
 }
